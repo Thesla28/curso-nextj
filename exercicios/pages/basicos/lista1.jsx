26b534abd35d922bea231115f94214bc5aee37e0
Hiago Silva <hiago2804@gmail.com>
2022-01-22 14:10:15 +0000

    First commit

---
+++ exercicios/pages/basicos/lista1.jsx
@@ -0,0 +1,40 @@
+export default function lista() {
+    return (
+        <div>
+        <span>1,</span>
+        <span>2,</span>
+        <span>3,</span>
+        <span>4,</span>
+        <span>5,</span>
+        <span>6,</span>
+        <span>7,</span>
+        <span>8,</span>
+        <span>9,</span>
+        <span>10,</span>
+    </div>
+    )
+}
+
+
+/* MINHA VERSÃO
+export default function lista1() {
+    const lista1 = [
+        1,2,3,4,5,6,7,8,9,10
+    ];
+
+    return  (
+        <div>
+            <span>{lista1[0]},</span>
+            <span>{lista1[1]},</span>
+            <span>{lista1[2]},</span>
+            <span>{lista1[3]},</span>
+            <span>{lista1[4]},</span>
+            <span>{lista1[5]},</span>
+            <span>{lista1[6]},</span>
+            <span>{lista1[7]},</span>
+            <span>{lista1[8]},</span>
+            <span>{lista1[9]},</span>
+        </div>
+    )
+}
+*/
