fc8fc7f83e1f9a3bcc03fa5192e1f4383f159bca
Hiago Silva <hiago2804@gmail.com>
2022-01-26 20:24:26 +0000

    Comunicacao Direta

---
+++ exercicios/components/direta/Pai.jsx
@@ -0,0 +1,11 @@
+import Filho from "./Filho";
+
+export default function Pai() {
+    return (
+        <div>
+            <Filho familia="Albuquerque"/>
+            <Filho familia="Albuquerque"/>
+            <Filho familia="Albuquerque"/>
+        </div>
+    )
+}
---
+++ exercicios/comunicacao/direta.jsx
@@ -0,0 +1,7 @@
+export default function Direta() {
+    return (
+        <div>
+            <h1>direta</h1>
+        </div>
+    )
+}
